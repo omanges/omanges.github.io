ceee85e3f173cae9dc4ec2670548fa515b39f41d
rishikesh <rishikeshgaikar07@gmail.com>
2017-12-31 11:51:34 +0530

    bc

---
+++ assets/js/typeomkar.js
@@ -0,0 +1,14 @@
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('Hello World!')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Strings can be removed')
+    .pauseFor(2500)
+    .deleteChars(7)
+    .typeString('altered!')
+    .start();
